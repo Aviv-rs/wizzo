5d6844b55128d7210350096d6ce3660dd2b42d0b
Aviv <aviv80010@gmail.com>
2022-08-25 11:31:42 +0300

    added basic header styling, added typography font

--- src/cmps/AppHeader.vue
+++ src/cmps/AppHeader.vue
@@ -1,10 +1,20 @@
 <template>
   <header class="app-header">
+    <div class="logo">
+      <img src="@/assets/imgs/header-logo.png" alt="" />
+    </div>
     <nav class="links flex">
-      <RouterLink v-for="link in links" :key="link" to="/">{{
+      <RouterLink class="link" v-for="link in links" :key="link" to="/">{{
         link
       }}</RouterLink>
     </nav>
+
+    <button class="btn-account flex align-center">
+      <div class="icon">
+        <img src="@/assets/imgs/smiley-icon.png" alt="" />
+      </div>
+      <span> איזור אישי </span>
+    </button>
   </header>
 </template>
 
