580805ce1a563d98aaf0c119219e1603e1547bb3
Aviv <aviv80010@gmail.com>
2022-08-25 12:02:57 +0300

    added basic hero styling

---
+++ src/cmps/Hero.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="hero grid">
+    <div class="main-hero-container relative">
+      <div class="welcome-text-container absolute">
+        <h3>סטודנטים באורט? ברוכים הבאים הביתה!</h3>
+        <h1>
+          כל מה שצריך לדעת
+          <br />
+          <span class="bold"> במקום אחד </span>
+        </h1>
+        <div class="buttons flex">
+          <button class="btn-signup clean-btn">הצטרפו לאגודה!</button>
+          <button class="btn-login clean-btn">
+            או <span class="bold">לחצו להתחברות</span>
+          </button>
+        </div>
+      </div>
+      <div class="main-hero-img">
+        <img src="@/assets/imgs/hero-1.png" alt="" />
+      </div>
+    </div>
+    <div class="hero-img1">
+      <img src="@/assets/imgs/hero-img1.png" alt="" />
+    </div>
+    <div class="hero-img2">
+      <img src="@/assets/imgs/hero-img2.png" alt="" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <template>
-  <section class="home-page flex column main-layout"></section>
+  <section class="home-page flex column main-layout">
+    <hero></hero>
+  </section>
 </template>
 
 <script>
+import Hero from '../cmps/Hero.vue'
 export default {
+  components: { Hero },
   data() {
     return {
       articles: null,
